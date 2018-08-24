dcce00dd105b58be1fcd424f4db7309efa65e83a
Heather <hlmathies@gmail.com>
2018-08-24 14:51:00 -0400

    preparing file structure

---
+++ src/app/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>CAPCO</h1>
+        <p><strong>THE FUTURE.NOW</strong></p>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './app/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const rootElement = document.getElementById('root');
+
+let render = () => {
+    ReactDOM.render(<App />, rootElement)
+}
+if (module.hot) {
+    module.hot.accept('./App', () => {
+        setTimeout(render)
+    })
+}
+render();
 registerServiceWorker();
